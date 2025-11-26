67feec309218734e4d515ff6b6fd90a120806d8d
Zane Starr <zcstarr@gmail.com>
2025-11-26 09:20:46 -0800

    fix: add additional styling to the scheam components mock render

--- packages/docusaurus-plugin/src/components/SchemaComponent/SchemaComponent.tsx
+++ packages/docusaurus-plugin/src/components/SchemaComponent/SchemaComponent.tsx
@@ -1,5 +1,6 @@
 import React, { PropsWithChildren } from "react";
+import styles from "./style.module.css";
 
 export const SchemaComponent: React.FC<PropsWithChildren> = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className={styles.schemaComponent}>{children}</div>;
 };
