5bb84e8c4ff2e116fb631e5255738d3ac6f24eec
Zane Starr <zcstarr@gmail.com>
2025-11-26 17:52:17 -0800

    feat: wip components.mdx

--- packages/docusaurus-plugin/src/components/TryNow/TryNowDialog/TryNowDialog.tsx
+++ packages/docusaurus-plugin/src/components/TryNow/TryNowDialog/TryNowDialog.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { type PropsWithChildren } from "react";
+import {
+  RequestExample,
+  ResponseExample,
+  TwoColumnLayout,
+} from "../../ApiExamples";
 type IParams = {
   openrpcDocument: any;
   method: string;
   ref: React.RefObject<HTMLDialogElement | null>;
   onClose: () => void;
-};
+} & PropsWithChildren;
 
 export const TryNowDialog: React.FC<IParams> = (params) => {
   return (
@@ -12,7 +17,22 @@ export const TryNowDialog: React.FC<IParams> = (params) => {
       <h2>Try It Now Super Dialog</h2>
       <form method="dialog" onSubmit={() => params.ref.current?.close()}>
         <button type="submit" onClick={params.onClose}>
-          Close
+          {/*          <TwoColumnLayout
+            sidebar={
+              <>
+                <RequestExample
+                  code={JSON.stringify(params.openrpcDocument.params, null, 2)}
+                />
+                <ResponseExample
+                  code={JSON.stringify(params.openrpcDocument.result, null, 2)}
+                />
+              </>
+            }
+          >
+            {params.children ?? <div>main</div>}
+          </TwoColumnLayout>
+          */}
+          {params.children ?? <div>main</div>}
         </button>
       </form>
     </dialog>
